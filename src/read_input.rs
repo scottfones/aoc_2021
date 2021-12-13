dd8820975536bb03c2d0e30f4b313d0e5ff0db6d
Scott Fones <scott.fones@gmail.com>
2021-12-13 04:12:12 -0500

    removed unnecessary ref on filename

--- src/read_input.rs
+++ src/read_input.rs
@@ -6,7 +6,7 @@ use std::path::Path;
 
 pub fn read_file(filename: &str) -> Vec<String> {
     let mut input = Vec::new();
-    if let Ok(lines) = read_lines("./input/".to_string() + &filename) {
+    if let Ok(lines) = read_lines("./input/".to_string() + filename) {
         for line in lines {
             input.push(line.unwrap());
         }
